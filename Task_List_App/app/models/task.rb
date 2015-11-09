742c9670604f008aceadb8d6a52155dfcd6b2006
Kevin Boyd <kevinisverycool@yahoo.com>
2015-11-09 18:43:50 -0500

    'validations'

--- Task_List_App/app/models/task.rb
+++ Task_List_App/app/models/task.rb
@@ -1,5 +1,8 @@
 class Task < ActiveRecord::Base
 	belongs_to :tasklist
+	validates :title, :priority, :duedate, :tasklist_id, presence: { message: "must be present" }
+	validates :priority, numericality: true
+	validates :priority, inclusion: { in: 1..10, message: 'needs to be between 1 and 10'  }
 	
 	def to_s
 		title
--- Task_List_App/app/models/tasklist.rb
+++ Task_List_App/app/models/tasklist.rb
@@ -1,6 +1,7 @@
 class Tasklist < ActiveRecord::Base
 	has_many :tasks
-	
+	validates :name, :description, presence: { message: "must be present" }
+	validates :name, uniqueness: true
 	def to_s
 		name
 	end
