d67b0eea62c8f40b2e77cce064da9a34b066bfd4
Kevin Boyd <kevinisverycool@yahoo.com>
2015-12-02 23:11:12 -0500

    layout final

--- Task_List_App/app/controllers/todo_list_controller.rb
+++ Task_List_App/app/controllers/todo_list_controller.rb
@@ -6,13 +6,8 @@ class TodoListController < ApplicationController
 	if !params.nil?
 		if params.has_key?(:id)
 			@tasklist = Tasklist.find(params[:id])
-		else
-			@tasklist = Tasklist.find(1)
 		end
-	else 
-		@tasklist = Tasklist.find(1)
-		
 	end
   end
-  
+
  end
